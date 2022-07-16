bc6e7bd0b21365ba014c9efd34f6e830d1df9afd
juujoojaa <thiscantbewhy@gmail.com>
2022-07-16 09:30:45 +0300

    player movement added. speed can be changed. try it out people pls. and give me feedback. ty. yay.

---
+++ Assets/Scripts/AlsoPlayerMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlsoPlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float jumpSpeed;
+    private Rigidbody2D body;
+    public bool isJumping;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+
+        if (Input.GetKey(KeyCode.Space) && isJumping == false)
+            {
+            body.velocity = new Vector2(body.velocity.x, jumpSpeed);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = true;
+        }
+    }
+}
