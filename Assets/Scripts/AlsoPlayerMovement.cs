b60c3155326527917389cd2891b743ca32348e6e
Renkku95 <anna.kpuos@hotmail.com>
2022-07-17 12:25:04 +0300

    idle, run ja jump animaatiot
    pelaajan koko scenessä täytyy korjata

--- Assets/Scripts/AlsoPlayerMovement.cs
+++ Assets/Scripts/AlsoPlayerMovement.cs
@@ -8,26 +8,48 @@ public class AlsoPlayerMovement : MonoBehaviour
     [SerializeField] float jumpSpeed;
     private Rigidbody2D body;
     public bool isJumping;
+    private Animator anim;
 
     private void Awake()
     {
+        //Grab references for rigidbody and animator from object
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Start()
     {
         body.freezeRotation = true;
+        
     }
 
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        //Flip player when moving
+        if (horizontalInput > 0.01f)
+            {
+            transform.localScale = Vector3.one;
+        }
+        else if (horizontalInput < -0.01f)
+            {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
 
         if (Input.GetKey(KeyCode.Space) && isJumping == false)
             {
             body.velocity = new Vector2(body.velocity.x, jumpSpeed);
+            anim.SetTrigger("Jump");
+            isJumping = true;
         }
+
+        //Set animator parameters
+        anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("isjumping", isJumping);
     }
+
 
     private void OnCollisionEnter2D(Collision2D other)
     {
